f89c0e4b2d001cd20c49d7ac517baa6238857861
yashmit7 <139227779+yashmit7@users.noreply.github.com>
2026-04-19 16:36:41 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,60 @@
+num1 = int(input("enter the first number:"))
+num2 = int(input("enter the second numebr:"))
+
+operation = input("select one of the following- add, subtract, multiply, divide, remainder:")
+
+if operation == "add":
+    print("adding the two digits:", num1 + num2)
+elif operation == "subtract":
+    print("subtracting the digits:", num1 - num2)
+elif operation == "multiply":
+    print("multiplying the digits:", num1 * num2)
+elif operation == "divide":
+    print("dividing the digits:", num1 / num2)
+elif operation == "remainder":
+    print("remainder of the digits:", num1 % num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
